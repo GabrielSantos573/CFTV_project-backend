8f21602ea00ee50115183f98619dd3fb3485f152
Gabriel Oliveira Santos <80365261+GabrielSantos573@users.noreply.github.com>
2024-06-05 16:12:48 -0300

    Add files via upload

---
+++ backend/app.py
@@ -0,0 +1,24 @@
+#.\venv\Scripts\activate.bat
+from flask import Flask, jsonify
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config.from_object(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cftv.db'
+
+db = SQLAlchemy(app)
+
+CORS(app, resources={r"/*":{'origins': 'http//localhost:8080', "allow_headers":
+    "Acess-Control-Allow-Origin"}})
+
+@app.route("/", methods=['GET'])
+def hello_world():
+    return "Hello, World!"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
+    
---
+++ backend/models.py
@@ -0,0 +1,9 @@
+from app import db
+
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, unique=True , nullable=False)
+    senha = db.Column(db.String, nullable=False)
+    
+    
